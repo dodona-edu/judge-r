4dc60a08edb9f275f7c45db04b4225474c0e11a3
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-06-26 09:54:23 +0200

    Don't stop test execution on warnings or messages

--- test.R
+++ test.R
@@ -1,24 +1,28 @@
 test_equal <- function(description, generated, expected) {
     get_reporter()$start_test(expected, description)
 
-    tryCatch({
-        expected_val <- expected
-        generated_val <- generated(test_env$clean_env)
-        if (expected_val == generated_val) {
-            get_reporter()$end_test(generated_val, "correct")
-        } else {
-            get_reporter()$end_test(generated_val, "wrong")
+    tryCatch(
+        withCallingHandlers(
+            {
+                expected_val <- expected
+                generated_val <- generated(test_env$clean_env)
+                if (expected_val == generated_val) {
+                    get_reporter()$end_test(generated_val, "correct")
+                } else {
+                    get_reporter()$end_test(generated_val, "wrong")
+                }
+            },
+            warning = function(w) {
+                get_reporter()$add_message(paste("Warning while evaluating test: ", w, sep = ''))
+            },
+            message = function(m) {
+                get_reporter()$add_message(paste("Message while evaluating test: ", m, sep = ''))
+            }
+        ),
+        error = function(e) {
+            get_reporter()$end_test("", "wrong")
+            get_reporter()$start_test("", description)
+            get_reporter()$end_test(e, "runtime error")
         }
-    },
-    error = function(e) {
-        get_reporter()$end_test("", "wrong")
-        get_reporter()$start_test("", description)
-        get_reporter()$end_test(e, "runtime error")
-    },
-    warning = function(w) {
-        get_reporter()$add_message(paste("Warning while evaluating test: ", w, sep = ''))
-    },
-    message = function(m) {
-        get_reporter()$add_message(paste("Message while evaluating test: ", m, sep = ''))
-    })
+    )
 }
