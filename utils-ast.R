23d44bc77f6b1f8cda675ceeeb15595a6ca519c5
gubogaer <gust.bogaert@ugent.be>
2020-09-07 15:20:43 +0200

    code cleanup + small fixes
    bug fixed: error when student defines function
    
    addition: ast testing in contextWithImage is now possible
    
    code cleanup

--- utils-ast.R
+++ utils-ast.R
@@ -1,52 +1,35 @@
 is_function_used_in_var <- function(func_name, var_paths, var, main_code) {
     var_paths_c <- var_paths
     var_paths_c[[var]] <- NULL
-    any(
-        lapply(var_paths[[var]],
-               function(x) is_function_used(func_name, var_paths_c, get_node(x, main_code), main_code)
-               ) == TRUE
-    )
+    return(some(var_paths[[var]], function(x) is_function_used(func_name, var_paths_c, get_node(x, main_code), main_code)))
 }
 
 is_function_used <- function(func_name, var_paths, sub_tree_root, main_code){
     switch(expr_type(sub_tree_root),
-           constant = { FALSE },
+           constant = { return(FALSE) },
            symbol = {
                var_paths_c <- var_paths
                var_paths_c[[sub_tree_root]] <- NULL
-               any(
-                   lapply(var_paths[[sub_tree_root]],
-                          function(x) is_function_used(func_name, var_paths_c, get_node(x, main_code), main_code)
-                          ) == TRUE
-               )
+               return(some(var_paths[[sub_tree_root]], function(x) is_function_used(func_name, var_paths_c, get_node(x, main_code), main_code)))
            },
            call = {
                if (is_call(sub_tree_root, func_name)) {
                    return(TRUE)
                }
                children <- as.list(sub_tree_root)
+               # No need to check assignation branches of parameters before assignation symbol
                if(is_call(sub_tree_root, "<-") && is_symbol(children[[2]])){
                    children[[2]] <- NULL
                }
-               any(
-                   lapply(children,
-                          function(x) is_function_used(func_name, var_paths, x, main_code)
-                          ) == TRUE
-               )
+               return(some(children, function(x) is_function_used(func_name, var_paths, x, main_code)))
            },
            expression = {
-               any(
-                   lapply(as.list(sub_tree_root),
-                          function(x) is_function_used(func_name, var_paths, x, main_code)
-                          ) == TRUE
-               )
+               return(some(sub_tree_root, function(x) is_function_used(func_name, var_paths, x, main_code)))
            },
            pairlist = {
-               any(
-                   lapply(as.list(sub_tree_root),
-                          function(x) is_function_used(func_name, var_paths, x, main_code)
-                          ) == TRUE
-               )
+               return(some(sub_tree_root, function(x) {
+                               !(is.symbol(x) && identical("", as.character(x))) && is_function_used(func_name, var_paths, x, main_code)
+}))
            },
            stop("Don't know how to handle type ", typeof(sub_tree_root), call. = FALSE)
     )
@@ -77,23 +60,24 @@ expr_type <- function(x) {
 }
 
 concat_lists <- function(list1, list2){
+    if(identical(list1, NULL)){
+        return(list2)
+    }
+    if(identical(list2, NULL)){
+        return(list1)
+    }
     keys <- unique(c(names(list1), names(list2)))
     for (key in keys){
+        if(identical(list1[[key]], NULL)){
+            list1[[key]] <- list()
+        }
         for (index in seq_along(list2[[key]])){
             list1[[key]][[length(list1[[key]])+1]] <- list2[[key]][[index]]
             list1[[key]] <- unique(list1[[key]])
         }
     }
-    list1
+    return(list1)
 }
-concat_lists1 <- function(lists){
-    result = lists[1]
-    for(index in 2:length(lists)){
-        result = concat_lists(result, lists[index])
-    }
-    result
-}
-
 
 find_assign <- function(x, start_path=c()) {
     switch(expr_type(x),
@@ -104,34 +88,18 @@ find_assign <- function(x, start_path=c()) {
                return(NULL)
            },
            call = {
-               res <- c()
+               assignation_path <- NULL
                if (is_call(x, "<-") && is_symbol(x[[2]])) {
-                   lhs <- as_string(x[[2]])
-                   assignation_paths <- list()
-                   assignation_paths[[lhs]] <- list(start_path)
-                   res <- c(assignation_paths)
+                   assignation_path <- list()
+                   assignation_path[[as_string(x[[2]])]] <- list(start_path)
                }
-               for(child_index in seq_along(x)){
-                   path <- c(start_path, child_index)
-                   res <- c(res, find_assign(x[[child_index]], path))
-               }
-               return(concat_lists1(res))
+               return(Reduce(concat_lists, lapply(seq_along(x), function(index) find_assign(x[[index]], c(start_path, index))), assignation_path))
            },
            pairlist = {
-               res <- c()
-               for (index in seq_along(x)){
-                   path <- c(start_path, index)
-                   res <- c(res, find_assign(x[[index]], path))
-               }
-               return(concat_lists1(res))
+               return(NULL)
            },
            expression = {
-               res <- c()
-               for (index in seq_along(x)){
-                   path <- c(start_path, index)
-                   res <- c(res, find_assign(x[[index]], path))
-               }
-               return(concat_lists1(res))
+               return(Reduce(concat_lists, lapply(seq_along(x), function(index) find_assign(x[[index]], c(start_path, index))), NULL))
            },
            stop("Don't know how to handle type ", typeof(x), call. = FALSE)
     )
