b720364f333e5574bff64c0b923eb9c5ef9375cd
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-09-10 09:31:18 +0200

    Implement testIdentical in function of testEqual

--- test.R
+++ test.R
@@ -171,43 +171,13 @@ testGGPlot <- function(description, generated, expected, test_data = TRUE, test_
 }
 
 testIdentical <- function(description, generated, expected, formatter = NULL, ...) {
-    if (is.null(formatter)) {
-        expected_formatted <- expected
-    } else {
-        expected_formatted = formatter(expected)
-    }
-    get_reporter()$start_test(expected_formatted, description)
-
-    tryCatch(
-             withCallingHandlers({
-                 expected_val <- expected
-                 generated_val <- generated(test_env$clean_env)
-
-                 equal <- isTRUE(identical(generated_val, expected_val, ...))
-
-                 if (is.null(formatter)) {
-                     generated_formatted <- generated_val
-                 } else {
-                     generated_formatted = formatter(generated_val)
-                 }
-
-                 if (equal) {
-                     get_reporter()$end_test(generated_formatted, "correct")
-                 } else {
-                     get_reporter()$end_test(generated_formatted, "wrong")
-                 }
-             },
-             warning = function(w) {
-                 get_reporter()$add_message(paste("Warning while evaluating test: ", conditionMessage(w), sep = ''))
-             },
-             message = function(m) {
-                 get_reporter()$add_message(paste("Message while evaluating test: ", conditionMessage(m), sep = ''))
-             }),
-             error = function(e) {
-                 get_reporter()$end_test("", "wrong")
-                 get_reporter()$start_test("", description)
-                 get_reporter()$end_test(conditionMessage(e), "runtime error")
-             }
+    testEqual(
+              description,
+              generated,
+              expected,
+              comparator = function(generated_val, expected_val, ...) { isTRUE(identical(generated_val, expected_val, ...)) },
+              formatter = formatter,
+              ...
     )
 }
 
